1f30d49d667ad9fbcc770ffd18a7eb2731d4b9cb
danillo <danillogonvalves001@gmail.com>
2022-01-11 22:31:47 -0300

    Exercicios 14 finalizados.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const loginEmailTrybe = document.querySelector('#login-email');
 const loginSenhaTrybe = document.querySelector('#login-senha');
 const loginButtonTrybe = document.querySelector('#login-button');
+const containerAvaliacao = document.querySelector('#container-avaliacao');
 
 const entrar = function loginTrybewarts(event) {
   event.preventDefault();
@@ -10,5 +11,17 @@ const entrar = function loginTrybewarts(event) {
     alert('Email ou senha inválidos.');
   }
 };
+
+for (let i = 1; i <= 10; i += 1) {
+  const radio = document.createElement('input');
+  const label = document.createElement('label');
+  radio.type = 'radio';
+  radio.name = 'rate';
+  radio.id = `rate${i}`;
+  label.htmlFor = `rate${i}`;
+  label.appendChild(radio);
+  label.innerHTML += i;
+  containerAvaliacao.appendChild(label);
+}
 
 loginButtonTrybe.onclick = entrar;
